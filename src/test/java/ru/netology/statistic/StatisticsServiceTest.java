8971c10fecad17311f70f71df0054cc400434577
Zharova Yulya <zharovayulya1997@gmail.com>
2023-09-04 20:37:40 +0500

    test verify

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,7 +17,7 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-   /* @Test
+   @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
@@ -27,5 +27,5 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
-    }*/
+    }
 }
